c445e99cc274276f50119847dc9479ba3cfe6aa9
Leonardo12SH <sulaimanhamzah12@gmail.com>
2024-08-10 23:57:03 +0800

    update meta data

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -19,7 +19,7 @@ Desa Bonto Salama
                                     Potensi</a></div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-6">
+                    <div class="col-12 col-md-6 col-lg-6 mb-5">
                         <div class="hero-img">
                             <img src="image.png" alt="Peta Desa Bonto Salama" height="100%">
                         </div>
@@ -27,7 +27,7 @@ Desa Bonto Salama
                 </div>
             </div>
             <div class="shape-divider-bottom">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1100 90" preserveAspectRatio="none">
                     <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
                 </svg>
             </div>
